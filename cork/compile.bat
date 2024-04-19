6964c7dea8d287b302f1289668b4c86f20675a1f
BorisKlco <klco@pm.me>
2024-04-19 14:58:10 +0200

    cork echo

---
+++ cork/compile.bat
@@ -0,0 +1,7 @@
+REM compile Cork
+@echo off
+echo ca65:
+C:\Users\klco\Downloads\cc65\bin\ca65.exe -D cork cork.s -o cork.o
+echo ld65:
+C:\Users\klco\Downloads\cc65\bin\ld65.exe -C cork.cfg cork.o -o cork.bin -Ln cork.lbl
+pause > nul
--- cork/init.s
+++ cork/init.s
@@ -8,16 +8,33 @@ RESET:
  sta ACIA_CMD
  cli
 ESCAPE:
- lda #$5C
+ lda #$5C ; "\"
  jsr CHROUT
 NEW_LINE:
- lda #$0D
+ lda #$0D ; CR
  jsr CHROUT
- lda #$0A
+ lda #$0A ; LF
  jsr CHROUT
+ENTER:
+ ldx WRITE_PTR
+ lda INPUT_BUFFER, x
+ cmp #$0D
+ beq PRINT
+ jmp ENTER
 BREAK:
  nop
  jmp BREAK
+PRINT:
+ ldy #$00
+@print_loop:
+ lda INPUT_BUFFER, y
+ jsr CHROUT
+ cpy WRITE_PTR
+ beq NEW_LINE
+ iny
+ jmp @print_loop
+ 
+ 
 
 
  
